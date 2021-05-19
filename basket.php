1ac2e6079b4c101dc1c1a53f61c5ab8cad1026f9
Thisath Thiyunajith <thisaththiyunajith@gmail.com>
2021-05-19 23:57:39 +0530

    Adding remove button for the basket

--- basket.php
+++ basket.php
@@ -1,7 +1,7 @@
 <?php 
 
 include("db.php");
-
+session_start();
 $pagename="smart basket";               //Create and populate a variable called $pagename 
 echo "<link rel=stylesheet type=text/css href=mystylesheet.css>";  //Call in stylesheet 
  
@@ -13,26 +13,43 @@ include ("headfile.html");              //include header layout file
  
 echo "<h4>".$pagename."</h4>";          //display name of the page on the web page 
 
-//capture the ID of selected product using the POST method and the $_POST superglobal variable 
+// Remove from the basket
+if (isset($_POST['delnb']))   // //if the value of the product id to be deleted (which was posted through the hidden field) is set
+{
+$delprodId=$_POST['delnb'];//capture the posted product id and assign it to a local variable $delprodid
+unset($_SESSION['basket'][$delprodId]);//unset the cell of the session for this posted product id variable
+}
+echo "1 item removed from the basket<br>";//display a "1 item removed from the basket" message
+
+$to =0; 
+if (isset($_POST['h_prodid'])) { 
+
+//capture the ID of selected product using the POST method and the $_POST superglobal variable
 //and store it in a new local variable called $newprodid
 $newprodid=$_POST['h_prodid'];
 
-//capture the required quantity of selected product using the POST method and $_POST superglobal variable  
+//capture the required quantity of selected product using the POST method and $_POST superglobal variable
 //and store it in a new local variable called $reququantity
-$reququantity = $_POST['p_quality'];
 
-//Display id of selected product
-//echo "<p>"."Selected product ID: ".$newprodid."<br>";
-//Display quantity of selected product
-//echo "<p>"."Quentity of selected product: ".$reququantity."<br>";
+$reququantity =$_POST['p_quantity'];
 
-//create a new cell in the  basket session array. Index this cell with the new product id. 
-//Inside the cell store the required product quantity  
-if (isset($newprodid)){
-    $_SESSION['basket'][$newprodid]=$reququantity; 
-    echo "<p><b>1 item added</b>"."<br>"; 
-}else {
-    echo "<p><b>Basket Uncahnged...</b>"."<br>";
+
+
+//display the value of the product id, for debugging purposes
+//echo "<p> ID of selected product : ".$newprodid;
+
+//Display quantity of selected product, for debugging purposes
+//echo"<p>Display quantity of selected product : ".$reququantity;
+
+//create a new cell in the basket session array. Index this cell with the new product id.
+//Inside the cell store the required product quantity
+$_SESSION['basket'][$newprodid]=$reququantity;
+echo "<p>1 item added <br>";
+
+}
+
+else { 
+    echo "Basket unchanged.... <br>"; 
 }
 
 echo "<table>";
